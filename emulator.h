de4d47b9738de08d95ab806a4b71e198a5300782
NakagamiYuta <guguru0014@gmail.com>
2019-11-06 23:38:12 +0900

    [change] emulator.hのコメントを変更した

--- emulator.h
+++ emulator.h
@@ -1,22 +1,25 @@
 #ifndef EMULATORH
 #define EMULATORH
 
+/* ヘッダファイル */
 #include <stdint.h>
 
+/* 定数 */
 #define REGISTERS_COUNT 8
 
+/* 構造体 */
 typedef struct {
 
-    /* 汎用レジスタ */
+    // 汎用レジスタ
     uint32_t registers[REGISTERS_COUNT];
 
-    /* EFLAGSレジスタ */
+    // EFLAGSレジスタ
     uint32_t eflags;
 
-    /* メモリ */
+    // メモリ
     uint8_t *memory;
 
-    /* EIPレジスタ(プログラムカウンタ) */
+    // EIPレジスタ(プログラムカウンタ)
     uint32_t eip;
 
 } Emulator;
