547abc9138b7b4789931a1f9484d59b286e57f8d
NakagamiYuta <guguru0014@gmail.com>
2019-11-06 23:57:47 +0900

    [add] main.c
    エミュ作成からレジスタ初期化, メモリ配置まで

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "emulator/emulator.h"
+
+#define MEMORY_SIZE 1000
+
+int main(int argc, char *argv[])
+{
+    FILE *binary;
+    Emulator *emu;
+
+    // 引数チェック
+    if(argc != 2) {
+        puts("Usage: ./main x86_filename");
+        return 1;
+    }
+
+    // エミュレータ作成
+    emu = create_emu(MEMORY_SIZE, 0x0000, 0x7c00);
+
+    // x86プログラムファイル読み込み
+    binary = fopen(argv[1], "rb");
+    if(binary == NULL) {
+        printf("x86 File not found! : %s\n", argv[1]);
+        return 1;
+    }
+
+    // メモリにx86プログラムを配置する(8バイト単位)
+    fread(emu->memory, 1, 0x200, binary);
+    fclose(binary);
+
+    destroy_emu(emu);
+    return 0;
+}
