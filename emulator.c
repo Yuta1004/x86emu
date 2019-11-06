a64dacdd51dd4ea69c7a15473171d2709fa7cd6c
NakagamiYuta <guguru0014@gmail.com>
2019-11-06 23:45:43 +0900

    [add] create_emu関数作成

---
+++ emulator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "emulator.h"
+
+/* エミュレータ作成 */
+Emulator *create_emu(size_t mem_size, uint32_t eip, uint32_t esp)
+{
+    // 初期化
+    Emulator *emu = malloc(sizeof(Emulator));
+    emu->memory = malloc(mem_size);
+
+    // レジスタ初期化
+    memset(emu->registers, 0, sizeof(emu->registers));
+    emu->registers[ESP] = esp;
+    emu->eip = eip;
+
+    return emu;
+}
+
+
