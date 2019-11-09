53107cef666288c0d5ee355fe38845c1ccae1678
NakagamiYuta <guguru0014@gmail.com>
2019-11-10 00:12:12 +0900

    [add] mov r32, rm32, mov rm32, r32を実行できるようにした

--- emulator/instruction.c
+++ emulator/instruction.c
@@ -4,23 +4,9 @@
 #include "emulator.h"
 #include "memory.h"
 #include "instruction.h"
+#include "modrm.h"
 
-/* オペコードとそれを処理する関数の対応表 */
-void init_instruction_table()
-{
-    memset(instructions, 0, sizeof(instructions));
-
-    // mov
-    for(int idx = 0; idx < 8; ++ idx)
-        instructions[0xB8+idx] = mov_r32_imm32;
-
-    // jmp
-    instructions[0xEB] = short_jmp;
-    instructions[0xE9] = near_jmp;
-}
-
-
-/* mov(32bit) */
+/* mov */
 void mov_r32_imm32(Emulator *emu)
 {
     uint8_t reg = get_code8(emu, 0) - 0xB8;
@@ -30,7 +16,25 @@ void mov_r32_imm32(Emulator *emu)
     return;
 }
 
-/* short jmp(8bit) */
+void mov_r32_rm32(Emulator *emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t val = get_rm32(emu, &modrm);
+    set_r32(emu, &modrm, val);
+}
+
+void mov_rm32_r32(Emulator *emu)
+{
+    emu->eip += 1;
+    ModRM modrm;
+    parse_modrm(emu, &modrm);
+    uint32_t val = get_r32(emu, &modrm);
+    set_rm32(emu, &modrm, val);
+}
+
+/* jmp */
 void short_jmp(Emulator *emu)
 {
     int8_t adiff = get_sign_code8(emu, 1);
@@ -39,11 +43,26 @@ void short_jmp(Emulator *emu)
     return;
 }
 
-/* near jmp(32bit) */
 void near_jmp(Emulator *emu)
 {
     int32_t adiff = get_sign_code32(emu, 1);
     emu->eip += 5;
     emu->eip += adiff;
     return;
+}
+
+/* オペコードとそれを処理する関数の対応表 */
+void init_instruction_table()
+{
+    memset(instructions, 0, sizeof(instructions));
+
+    // mov
+    for(int idx = 0; idx < 8; ++ idx)
+        instructions[0xB8+idx] = mov_r32_imm32;
+    instructions[0x89] = mov_rm32_r32;
+    instructions[0x8B] = mov_r32_rm32;
+
+    // jmp
+    instructions[0xEB] = short_jmp;
+    instructions[0xE9] = near_jmp;
 }
--- emulator/instruction.h
+++ emulator/instruction.h
@@ -10,9 +10,5 @@ extern instruction_func* instructions[256];
 
 /* 関数 */
 void init_instruction_table();
-void mov_r32_imm32(Emulator *emu);
-void short_jmp(Emulator *emu);
-void near_jmp(Emulator *emu);
-
 
 #endif
