b96f4ac08c312c730f81a314ae061ea2870d969d
NakagamiYuta <guguru0014@gmail.com>
2019-11-09 23:04:25 +0900

    [change] レジスタアクセス失敗時には強制終了するようにした

--- emulator/register.c
+++ emulator/register.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
 #include "emulator.h"
 
@@ -6,15 +7,19 @@
 /* レジスタ取得(32bit) */
 uint32_t get_reg32(Emulator *emu, int reg_num)
 {
-    if(reg_num >= REGISTERS_COUNT)
-        return 0;
+    if(reg_num >= REGISTERS_COUNT) {
+        printf("Failed to access to reg! => %d", reg_num);
+        exit(1);
+    }
     return emu->registers[reg_num];
 }
 
 /* レジスタ書き込み(32bit) */
 void set_reg32(Emulator *emu, int reg_num, uint32_t val)
 {
-    if(reg_num >= REGISTERS_COUNT)
-        return;
+    if(reg_num >= REGISTERS_COUNT) {
+        printf("Failed to access to reg! => %d", reg_num);
+        exit(1);
+    }
     emu->registers[reg_num] = val;
 }
