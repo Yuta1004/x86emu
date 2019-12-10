55f5b09fef2bf43e81b1aa150d60ea944ce879ac
NakagamiYuta <guguru0014@gmail.com>
2019-12-10 23:47:29 +0900

    [add] 実行終了時にEFLAGSの値を出力するようにした

--- emulator/emulator.c
+++ emulator/emulator.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "emulator.h"
+#include "register.h"
 
 /* エミュレータ作成 */
 Emulator *create_emu(size_t mem_size, uint32_t eip, uint32_t esp)
@@ -33,4 +34,9 @@ void display_reg_emu(Emulator *emu)
     for(int idx = 0; idx < 8; ++ idx)
         printf("%s: 0x%08x\n", regs[idx], emu->registers[idx]);
     printf("EIP: 0x%08x\n", emu->eip);
+    printf("EFLAGS:\n");
+    printf("    - CF: %d\n", get_flag(emu, CARRY_FLAG));
+    printf("    - ZF: %d\n", get_flag(emu, ZERO_FLAG));
+    printf("    - SF: %d\n", get_flag(emu, SIGN_FLAG));
+    printf("    - OF: %d\n", get_flag(emu, OVERFLOW_FLAG));
 }
--- emulator/register.h
+++ emulator/register.h
@@ -6,10 +6,11 @@
 #include "emulator.h"
 
 /* 定数 */
-#define CARRY_FLAG      (1 << 0)
-#define ZERO_FLAG       (1 << 6)
-#define SIGN_FLAG       (1 << 7)
-#define OVERFLOW_FLAG   (1 << 11)
+#define CARRY_FLAG          (1 << 0)
+#define ZERO_FLAG           (1 << 6)
+#define SIGN_FLAG           (1 << 7)
+#define OVERFLOW_FLAG       (1 << 11)
+#define get_flag(emu, POS)  ((((emu)->eflags)&(POS))>0)
 
 /* 関数 */
 uint32_t get_reg32(Emulator *emu, int reg_num);
