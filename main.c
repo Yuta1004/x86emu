db560289a3f8a7910e6214429a85ecb4c9a09411
NakagamiYuta <guguru0014@gmail.com>
2019-12-17 22:19:07 +0900

    [add] 実行時オプションに--varboseを追加

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 #include "emulator/emulator.h"
 #include "emulator/instruction.h"
 #include "emulator/memory.h"
@@ -8,12 +10,33 @@
 
 #define MEMORY_SIZE 0xffff
 
+int remove_opt_at(int argc, char *argv[], int arg_idx)
+{
+    if(arg_idx < 0 || argc <= arg_idx)
+        return argc;
+
+    for(int idx = arg_idx; idx < argc-1; ++ idx) {
+        argv[idx] = argv[idx+1];
+    }
+    argv[argc-1] = NULL;
+    return argc-1;
+}
+
 int main(int argc, char *argv[])
 {
     FILE *binary;
     Emulator *emu;
+    bool varbose = false;
     init_instruction_table();
     init_opecode_dec_table();
+
+    // 実行時オプションチェック
+    for(int arg_idx = 0; arg_idx < argc; ++ arg_idx) {
+        if(strncmp(argv[arg_idx], "--varbose", 2) == 0) {
+            varbose = true;
+            argc = remove_opt_at(argc, argv, arg_idx);
+        }
+    }
 
     // 引数チェック
     if(argc != 2) {
@@ -27,7 +50,8 @@ int main(int argc, char *argv[])
     // x86プログラムファイル読み込み
     binary = fopen(argv[1], "rb");
     if(binary == NULL) {
-        printf("x86 File not found! : %s\n", argv[1]);
+        if(varbose)
+            printf("x86 File not found! : %s\n", argv[1]);
         return 1;
     }
 
@@ -45,11 +69,13 @@ int main(int argc, char *argv[])
         }
 
         char *dec_str = opecode_dec_table[opecode];
-        printf("EIP: 0x%08x, OpeCode: 0x%02x, Inst: %s\n", emu->eip, opecode, dec_str);
+        if(varbose)
+            printf("EIP: 0x%08x, OpeCode: 0x%02x, Inst: %s\n", emu->eip, opecode, dec_str);
 
         instructions[opecode](emu);
         if(emu->eip == 0x00) {
-            puts("!!End of program!!");
+            if(varbose)
+                puts("!!End of program!!");
             break;
         }
     }
